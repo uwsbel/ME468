1bab4c52955371dbd9f996b006b390c64adbcdc9
DanNegrut <negrut@wisc.edu>
2022-02-06 21:21:01 -0600

    small

--- LecturesCode/numerical-methods/exampleFE-stiff.py
+++ LecturesCode/numerical-methods/exampleFE-stiff.py
@@ -1,11 +1,11 @@
-# Solves the IVP y_dot = -100*y and y(0)=0
+# Solves the IVP y_dot = -100*y and y(0)=1
 # yExact is the analytical solution
 # yFE is the solution obtained with Forward Euler
 #
 # Input: 
 # - the integration step-size
 # - end of the simulation time
-# - open window w/ plot, or generate png?
+# - plot as png or in a window?
 
 import numpy as np
 import matplotlib as plt
@@ -18,7 +18,9 @@ tend = float(sys.argv[2])
 pngYesNo = int(sys.argv[3])
 
 tm = np.arange(0,tend+h,h)
-yExact = np.exp(- 100 * tm)
+yExact = np.exp(- 100 * tm) 
+
+# use forward Euler
 yFE = np.zeros(tm.size)+1
 for i in np.arange(1,tm.size):
     yFE[i] = yFE[i - 1] * (1 - 100 * h)
@@ -27,11 +29,9 @@ for i in np.arange(1,tm.size):
 error = yExact - yFE
 print(error[0:6])
 
-# plot or png?
+# plot as png or in a window?
 plt.plot(tm,error)
 if pngYesNo == 1:
     plt.savefig("plot.png")
 else:
     plt.show()
-
-
--- LecturesCode/numerical-methods/exampleFE-stiffCosine.py
+++ LecturesCode/numerical-methods/exampleFE-stiffCosine.py
@@ -11,10 +11,11 @@ import matplotlib as plt
 import matplotlib.pyplot as plt
 import sys
 
-#h = float(sys.argv[1])
-#tend = float(sys.argv[2])
-h = 0.01
-tend = 1.0
+# get input
+h = float(sys.argv[1])
+tend = float(sys.argv[2])
+pngYesNo = int(sys.argv[3])
+
 tm = np.arange(0,tend+h,h)
 yExact = 1. / 10001 * (100 * np.sin(tm) - np.cos(tm) + np.exp(- 100 * tm))
 yFE = np.zeros(tm.size)
@@ -22,6 +23,10 @@ for i in np.arange(1,tm.size):
     yFE[i] = yFE[i - 1] * (1 - 100 * h) + h * np.sin(tm[i - 1])
 
 error = yExact - yFE
+
+# plot as png or in window?
 plt.plot(tm,error)
-plt.show()
-#plt.savefig("plot.png")
+if pngYesNo == 1:
+    plt.savefig("plot.png")
+else:
+    plt.show()
